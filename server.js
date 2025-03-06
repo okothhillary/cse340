0dd6955d7dc0ec9ebd5aa66957cf701d4060845f
okothhillary <hobboltport@gmail.com>
2025-03-06 18:21:16 +0300

    styling

--- server.js
+++ server.js
@@ -18,16 +18,19 @@ const static = require("./routes/static")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "layouts/layout") //not at views root
-
+app.set("style", "css/style.css")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
-app.get("/", function(req, res) {
+
+app.get("/", function (req, res) {
   res.render("index", { title: "Home" })
 })
+
+
 
 /* ***********************
  * Local Server Information
