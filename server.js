c026ceef76ea51ff84b9ce607135bd7be9144f56
okothhillary <hobboltport@gmail.com>
2025-03-17 16:28:20 +0300

    building inventory routes

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 
 /* ***********************
@@ -26,9 +28,8 @@ app.set("style", "css/style.css")
 app.use(static)
 
 
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" })
-})
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
 
 
